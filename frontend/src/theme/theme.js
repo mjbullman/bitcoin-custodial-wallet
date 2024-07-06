3a2fbf5076de4a515770bef7e7d6acc1e7d3e9e8
Martin Bullman <martin.j.bullman@gmail.com>
2024-07-06 15:53:02 +0300

    Add MUI theme configuration with Roboto font
    Set up a custom Material-UI theme.
    
    - Integrated Google Fonts Roboto with different font weights.
    - Configured Material-UI theme with dark mode.
    - Set primary and secondary app colors.

---
+++ frontend/src/theme/theme.js
@@ -0,0 +1,27 @@
+'use client'
+
+import { Roboto } from 'next/font/google'
+import { createTheme } from '@mui/material/styles'
+
+const roboto = Roboto({
+    weight: ['300', '400', '500', '700'],
+    subsets: ['latin'],
+    display: 'swap'
+})
+
+const theme = createTheme({
+    typography: {
+        fontFamily: roboto.style.fontFamily,
+    },
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#2196f3'
+        },
+        secondary: {
+            main: '#00e5ff'
+        }
+    }
+})
+
+export default theme
