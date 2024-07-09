46eb24a7264b34a4fbd950cd3afd28da4d7d66f4
Martin Bullman <martin.j.bullman@gmail.com>
2024-07-09 13:27:40 +0300

    Implement Layout component
    Created the Layout component to serve as the main wrapper for the application

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,38 @@
+import * as React from 'react'
+
+import Theme from '@/theme/theme'
+import NavBar from '@/app/components/layout/NavBar'
+import CssBaseline from '@mui/material/CssBaseline'
+import { AuthProvider } from '@/context/authContextState'
+import { ThemeProvider } from '@mui/material/styles'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter'
+
+import {
+    Box, Container
+} from '@mui/material'
+
+const Layout = ({ children }) => {
+    return (
+        <html lang="en">
+            <body>
+                <AppRouterCacheProvider>
+                    <ThemeProvider theme={ Theme }>
+                        <AuthProvider >
+                            <CssBaseline/>
+
+                            <NavBar></NavBar>
+
+                            <Container>
+                                <Box sx={{ my: 4 }}>
+                                    { children }
+                                </Box>
+                            </Container>
+                        </AuthProvider>
+                    </ThemeProvider>
+                </AppRouterCacheProvider>
+            </body>
+        </html>
+    )
+}
+
+export default Layout
