3ae8be8afd904e310da87777ca3fee076ce963c1
Martin Bullman <martin.j.bullman@gmail.com>
2024-07-09 22:50:00 +0300

    Implement JWT generation and cookie setting
    JWT Generation: Implemented the generateToken function to
    create a JWT for a given user object. The token includes user
    ID, email, and name, signed with a secret key (JWT_SECRET)
    and set to expire based on the JWT_EXPIRES environment
    variable.
    
    Cookie Setting: Implemented the setTokenCookie function to
    set the generated JWT as a cookie in the HTTP response. The
    cookie is configured with a max age of 1 hour, is accessible via
    HTTP only, and is set for the root path of the application.

---
+++ backend/src/utils/jwtAuth.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken')
+const cookie = require('cookie')
+
+/**
+ * Generates a JWT token for the given user.
+ *
+ * @param {Object} user - The user object containing user details.
+ * @param {string} user.id - The ID of the user.
+ * @param {string} user.email - The email of the user.
+ * @param {string} user.name - The name of the user.
+ * @returns {string} The generated JWT token.
+ */
+const generateToken = (user) => {
+    return jwt.sign({
+        id: user.id, email: user.email, name: user.name
+    }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES
+    })
+}
+
+/**
+ * Sets the JWT token as a cookie in the response.
+ *
+ * @param {Object} res - The HTTP response object.
+ * @param {string} token - The JWT token to be set as a cookie.
+ */
+const setTokenCookie = (res, token) => {
+    const serializedToken = cookie.serialize('token', token, {
+        httpOnly: false,
+        secure: false,
+        maxAge: 60 * 60 * 1000,
+        path: '/',
+    })
+
+    res.setHeader('Set-Cookie', serializedToken)
+}
+
+module.exports = { generateToken, setTokenCookie }
