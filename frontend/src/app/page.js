e63ae918ba6ed30fc1186fb5fb4a27915b19cdae
Martin Bullman <martin.j.bullman@gmail.com>
2024-07-09 14:40:03 +0300

    Create Home component with introductory UI
    Created a new Home component to serve as the landing
    page for the application.

---
+++ frontend/src/app/page.js
@@ -0,0 +1,50 @@
+'use client'
+
+import React from 'react'
+
+import Link from 'next/link'
+import Steps from '@/app/components/sections/Steps'
+
+import {
+    Grid, Container, Typography, Button
+} from '@mui/material'
+
+const Home = () => {
+    return (
+        <Container>
+            <Typography variant="h2" component="h1"  color="primary" sx={{
+                p: 3, textAlign: 'center', typography: { xs: 'h5', sm: 'h4', md: 'h3' }
+            }}>
+
+                Exodus Custodial Wallet
+
+            </Typography>
+
+            <Grid container spacing={{ xs: 2, md: 3 }} marginTop="10">
+                <Grid xs={12} item>
+                    <Typography variant="h4" color="secondary" sx={{
+                        p: 4, textAlign: 'center', typography: { xs: 'h6', sm: 'h5', md: 'h4' }
+                    }}>
+
+                        Get started now!
+
+                    </Typography>
+                </Grid>
+
+                <Steps/>
+            </Grid>
+
+            <Grid display="flex" justifyContent="center" sx={{ pt: 2 }}>
+                <Link href={'signup'} passHref>
+                    <Button size="large" variant="contained" color="primary" sx={{ m: 3 }}>
+
+                        Get Started
+
+                    </Button>
+                </Link>
+            </Grid>
+        </Container>
+    )
+}
+
+export default Home
